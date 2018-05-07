ea11818fa8cc5186e15832e0753574a4b719cdc5
DeanPaul <83623378@qq.com>
2018-05-07 22:08:45 +0800

    format code

--- Sec.js
+++ Sec.js
@@ -11,35 +11,40 @@ class Sec {
         this.$register.build();
         this.methods = v.methods;
     }
-    scan(node) {
-      if(node === this.el || !node.getAttribute('sec-li')) {
 
-          for(let i = 0; i < node.children.length; i++) {
-            const _thisNode = node.children[i]
-            this.parseModel(_thisNode);
+    scan(node) {
+        if (node === this.el || !node.getAttribute('sec-li')) {
+
+            for (let i = 0; i < node.children.length; i++) {
+                const _thisNode = node.children[i]
+                this.parseModel(_thisNode);
 //            this.parseClass(node)
-            this.parseEvent(_thisNode)
-            if(_thisNode.children.length) {
-              arguments.callee(_thisNode)
+                this.parseEvent(_thisNode)
+                if (_thisNode.children.length) {
+                    arguments.callee(_thisNode)
+                }
             }
-          }
         } else {
 //          this.parseList(node)
         }
 
     }
-    getData(){ return this.data;}
-    transformToArray(node){
+
+    getData() {
+        return this.data;
+    }
+
+    transformToArray(node) {
         let isInput = node.tagName === 'INPUT';
         let _data = isInput ? node.value : node.innerText;
         return this.parseInnerText(_data);
     }
 
-    transformToString(array,key,value){
+    transformToString(array, key, value) {
         let result = '';
         let data = this.data;
         array.forEach((d) => {
-            result += (d.type === 'string' ? d.value : (key&&key===d.value.trim())?value:data[d.value.trim()])
+            result += (d.type === 'string' ? d.value : (key && key === d.value.trim()) ? value : data[d.value.trim()])
         })
         return result;
     }
@@ -48,7 +53,7 @@ class Sec {
         let isInput = node.tagName === 'INPUT';
         let array = this.transformToArray(node);
         let result = this.transformToString(array);
-        let setNodeValue = (isInput,value)=>{
+        let setNodeValue = (isInput, value) => {
             if (isInput) {
                 node.value = value;
             } else {
@@ -56,34 +61,37 @@ class Sec {
             }
         }
         array.forEach((d) => {
-            if(d.type === 'object') {
+            if (d.type === 'object') {
                 this.$register.regist(this.data, d.value.trim(), (old, now) => {
-                     let r = this.transformToString(array,d.value.trim(),now);
-                     setNodeValue(isInput,r);
+                    let r = this.transformToString(array, d.value.trim(), now);
+                    setNodeValue(isInput, r);
                 });
             }
         })
-        setNodeValue(isInput,result);
+        setNodeValue(isInput, result);
     }
-    parseEvent(){}
-    parseInnerText(str){
+
+    parseEvent() {
+    }
+
+    parseInnerText(str) {
         let begin = '{{'
         let end = '}}';
         let result = [];
-        while(str.indexOf(begin)>=0 && str.indexOf(end)>=0){
+        while (str.indexOf(begin) >= 0 && str.indexOf(end) >= 0) {
             result.push({
-                type:'string',
-                value:str.substring(0,str.indexOf(begin))
-                });
+                type: 'string',
+                value: str.substring(0, str.indexOf(begin))
+            });
             result.push({
-                type:'object',
-                value:str.substring(str.indexOf(begin)+2,str.indexOf(end))
-                });
+                type: 'object',
+                value: str.substring(str.indexOf(begin) + 2, str.indexOf(end))
+            });
             str = str.substring(str.indexOf(end) + 2);
         }
         result.push({
-            type:'string',
-            value:str
+            type: 'string',
+            value: str
         });
         return result;
     }
@@ -120,25 +128,26 @@ class Register {
             this.observer(route.obj, route.key, route.fn);
         });
     }
+
     observer(obj, k, callback) {
 
-            let old = obj[k];
-            if (!old) {
-                throw new Error('The key to observe is undefined.');
-            }
-
-            Object.defineProperty(obj, k, {
-                enumerable: true,
-                configurable: true,
-                get: () => old,
-                set: (now) => {
-                    if (now !== old) {
-                        callback.forEach((fn) => {
-                            fn(old, now);
-                        });
-                    }
-                    old = now;
-                },
-            });
+        let old = obj[k];
+        if (!old) {
+            throw new Error('The key to observe is undefined.');
         }
+
+        Object.defineProperty(obj, k, {
+            enumerable: true,
+            configurable: true,
+            get: () => old,
+            set: (now) => {
+                if (now !== old) {
+                    callback.forEach((fn) => {
+                        fn(old, now);
+                    });
+                }
+                old = now;
+            },
+        });
+    }
 }
